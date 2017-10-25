10d0cce027df5d9a2d417f843b82c1459b6cfaf8
Doller <fantasistadolls@yahoo.co.jp>
2017-10-25 14:46:51 +0900

    Update bluetooth.py

--- scapy/layers/bluetooth.py
+++ scapy/layers/bluetooth.py
@@ -303,6 +303,15 @@ class ATT_Prepare_Write_Response(Packet):
                     XLEShortField("offset", 0),
                     StrField("value", ""), ]
     
+class ATT_Execute_Write_Request(Packet):
+    name = "Execute Write Request"
+    fields_desc = [ ByteEnumField("flags", 1, {0:"Cancel", 1:"Execute"}),]
+    
+class ATT_Execute_Write_Response(Packet):
+    name = "Execute Write Response"
+    fields_desc = [ ]
+       
+    
 class SM_Hdr(Packet):
     name = "SM header"
     fields_desc = [ ByteField("sm_command", None) ]
@@ -759,6 +768,8 @@ bind_layers( ATT_Hdr,       ATT_Write_Response, opcode=0x13)
 bind_layers( ATT_Hdr,       ATT_Write_Command, opcode=0x52)
 bind_layers( ATT_Hdr,       ATT_Prepare_Write_Request, opcode=0x16)
 bind_layers( ATT_Hdr,       ATT_Prepare_Write_Response, opcode=0x17)
+bind_layers( ATT_Hdr,       ATT_Execute_Write_Request, opcode=0x18)
+bind_layers( ATT_Hdr,       ATT_Execute_Write_Response, opcode=0x19)
 bind_layers( ATT_Hdr,       ATT_Handle_Value_Notification, opcode=0x1b)
 bind_layers( L2CAP_Hdr,     SM_Hdr,            cid=6)
 bind_layers( SM_Hdr,        SM_Pairing_Request, sm_command=1)
